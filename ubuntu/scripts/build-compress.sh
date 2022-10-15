6a5f9953ddcb00da54c89715144743f1c975ac6b
Kenneth Davidson <xylene2016@protonmail.com>
2022-10-15 12:02:25 -0400

    Update build-compress.sh

--- ubuntu/scripts/build-compress.sh
+++ ubuntu/scripts/build-compress.sh
@@ -29,10 +29,19 @@ rm -f "tar-${_tar_ver}.tar.xz"
 ls -la --color "tar-${_tar_ver}"
 
 cd lz4
-find ./ -iname Makefile | xargs -I "{}" sed '/^PREFIX/s|= .*|= /usr|g' -i "{}"
-find ./ -iname Makefile | xargs -I "{}" sed '/^LIBDIR/s|= .*|= /usr/lib/x86_64-linux-gnu|g' -i "{}"
-find ./ -iname Makefile | xargs -I "{}" sed '/^prefix/s|= .*|= /usr|g' -i "{}"
-find ./ -iname Makefile | xargs -I "{}" sed '/^libdir/s|= .*|= /usr/lib/x86_64-linux-gnu|g' -i "{}"
+sed '/^PREFIX/s|= .*|= /usr|g' -i Makefile
+sed '/^LIBDIR/s|= .*|= /usr/lib/x86_64-linux-gnu|g' -i Makefile
+sed '/^prefix/s|= .*|= /usr|g' -i Makefile
+sed '/^libdir/s|= .*|= /usr/lib/x86_64-linux-gnu|g' -i Makefile
+sed '/^PREFIX/s|= .*|= /usr|g' -i lib/Makefile
+sed '/^LIBDIR/s|= .*|= /usr/lib/x86_64-linux-gnu|g' -i lib/Makefile
+sed '/^prefix/s|= .*|= /usr|g' -i lib/Makefile
+sed '/^libdir/s|= .*|= /usr/lib/x86_64-linux-gnu|g' -i lib/Makefile
+sed '/^PREFIX/s|= .*|= /usr|g' -i programs/Makefile
+sed '/^LIBDIR/s|= .*|= /usr/lib/x86_64-linux-gnu|g' -i programs/Makefile
+sed '/^prefix/s|= .*|= /usr|g' -i programs/Makefile
+sed '/^libdir/s|= .*|= /usr/lib/x86_64-linux-gnu|g' -i programs/Makefile
+sleep 1
 make V=1 all prefix=/usr libdir=/usr/lib/x86_64-linux-gnu
 rm -fr /tmp/lz4
 rm -fr /tmp/lz4*.tar*
@@ -59,10 +68,19 @@ rm -fr /tmp/lz4
 cd "${_tmp_dir}"
 rm -fr lz4
 cd zstd
-find ./ -iname Makefile | xargs -I "{}" sed '/^PREFIX/s|= .*|= /usr|g' -i "{}"
-find ./ -iname Makefile | xargs -I "{}" sed '/^LIBDIR/s|= .*|= /usr/lib/x86_64-linux-gnu|g' -i "{}"
-find ./ -iname Makefile | xargs -I "{}" sed '/^prefix/s|= .*|= /usr|g' -i "{}"
-find ./ -iname Makefile | xargs -I "{}" sed '/^libdir/s|= .*|= /usr/lib/x86_64-linux-gnu|g' -i "{}"
+sed '/^PREFIX/s|= .*|= /usr|g' -i Makefile
+sed '/^LIBDIR/s|= .*|= /usr/lib/x86_64-linux-gnu|g' -i Makefile
+sed '/^prefix/s|= .*|= /usr|g' -i Makefile
+sed '/^libdir/s|= .*|= /usr/lib/x86_64-linux-gnu|g' -i Makefile
+sed '/^PREFIX/s|= .*|= /usr|g' -i lib/Makefile
+sed '/^LIBDIR/s|= .*|= /usr/lib/x86_64-linux-gnu|g' -i lib/Makefile
+sed '/^prefix/s|= .*|= /usr|g' -i lib/Makefile
+sed '/^libdir/s|= .*|= /usr/lib/x86_64-linux-gnu|g' -i lib/Makefile
+sed '/^PREFIX/s|= .*|= /usr|g' -i programs/Makefile
+sed '/^LIBDIR/s|= .*|= /usr/lib/x86_64-linux-gnu|g' -i programs/Makefile
+sed '/^prefix/s|= .*|= /usr|g' -i programs/Makefile
+sed '/^libdir/s|= .*|= /usr/lib/x86_64-linux-gnu|g' -i programs/Makefile
+sleep 1
 make V=1 all prefix=/usr libdir=/usr/lib/x86_64-linux-gnu
 rm -fr /tmp/zstd
 rm -fr /tmp/zstd*.tar*
